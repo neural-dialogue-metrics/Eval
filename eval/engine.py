9a3f4562ffaf9b4db7ce737b05f00868c2283c83
cgsdfc <cgsdfc@126.com>
2019-05-07 10:41:15 +0800

    add timestamp checking in engine.
    only run when target is older than sources.

--- eval/engine.py
+++ eval/engine.py
@@ -1,6 +1,7 @@
 import itertools
 import logging
 import pprint
+from pathlib import Path
 
 from eval.config_parser import parse_models_and_datasets, parse_metrics
 from eval.exporter import Exporter
@@ -27,6 +28,16 @@ class Engine:
         self.force = force
         self.under_tests = parse_config(config)
 
+    def is_outdated(self, output: Path, under_test):
+        if not output.exists():
+            return True
+        filenames = self.loader.get_filenames(under_test).values()
+        for file in filenames:
+            if file.stat().st_mtime > output.stat().st_mtime:
+                logger.info('file {} is newer than {}'.format(file, output))
+                return True
+        return False
+
     def run(self):
         logger.info('save_dir: %s', self.exporter.save_dir)
         logger.info('config: %s', pprint.pformat(self.config))
@@ -34,12 +45,11 @@ class Engine:
         self.exporter.export_config(self.config)
         for under_test in self.under_tests:
             output_path = self.exporter.get_output_path(under_test)
-            if output_path.is_file() and not self.force:
-                logger.info('skipping existing file %s', output_path)
+            if not self.is_outdated(output_path, under_test) and not self.force:
+                logger.info('skipping up-to-date file %s', output_path)
                 continue
 
             logger.info('Running under_test: %r', under_test)
-            logger.info('Loading resources: %s', ', '.join(under_test.metric.requires))
             payload = self.loader.load_resources(under_test)
             if payload is None:
                 continue
