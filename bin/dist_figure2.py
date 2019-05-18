aaf49b5ae5edb07869bf96e7cd46d8e4dabc81cf
cgsdfc <cgsdfc@126.com>
2019-05-18 17:12:20 +0800

    make figure smaller.

--- bin/dist_figure2.py
+++ bin/dist_figure2.py
@@ -4,15 +4,17 @@ from corr.normalize import normalize_name
 from pylatex import Figure, Label, Marker, NoEscape
 
 
-def make_figures(plot_prefix: Path, dst_prefix: Path):
+def make_figures(plot_prefix: Path, dst_prefix: Path, fig_width: float):
     files = list(plot_prefix.rglob('*.pdf'))
+    width_arg = r'{}\textwidth'.format(fig_width)
+    width_arg = NoEscape(width_arg)
 
     for file in files:
         metric = file.parent.name
         norm_metric = normalize_name('metric', metric)
         output = dst_prefix.joinpath(metric).with_suffix('.tex')
         figure = Figure(position='H')
-        figure.add_image(filename=str(file))
+        figure.add_image(filename=str(file), width=width_arg)
         figure.add_caption('{} 的概率分布图'.format(norm_metric))
         figure.append(Label(Marker(
             prefix='fig',
@@ -20,6 +22,8 @@ def make_figures(plot_prefix: Path, dst_prefix: Path):
         )))
         output.write_text(figure.dumps())
 
+
+FIGURE_WIDTH = 0.6
 
 if __name__ == '__main__':
     plot_prefix = Path('/home/cgsdfc/Metrics/Eval/data/v2/plot/distplot_grid')
@@ -30,4 +34,5 @@ if __name__ == '__main__':
     make_figures(
         plot_prefix=plot_prefix,
         dst_prefix=dst_prefix,
+        fig_width=FIGURE_WIDTH,
     )
