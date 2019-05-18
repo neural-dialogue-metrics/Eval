e3a10a6b4f3074456da157205ecff9d5ddddcc7f
cgsdfc <cgsdfc@126.com>
2019-05-18 14:25:44 +0800

    generate distplot grids.

--- bin/dist_figure.py
+++ bin/dist_figure.py
@@ -1,10 +1,13 @@
+import argparse
+import math
 from pathlib import Path
 
 import pandas
 from pandas import DataFrame
-from pylatex import Figure, SubFigure, Document, NewLine
+from pylatex import Figure, SubFigure, Document, NewLine, NoEscape, Command, Label, Marker
 
 from corr.consts import PLOT_FILENAME
+from corr.normalize import normalize_name
 
 
 def get_output(prefix: Path, metric):
@@ -24,29 +27,58 @@ def load_distplot_data(prefix: Path):
         )
 
     df = DataFrame.from_records(map(parse, files))
-    return df[(df.model != 'random') & (df.metric != 'serban_ppl')]
+    df = df[(df.model != 'random') & (df.metric != 'serban_ppl')]
+    df = df.sort_values(by=['metric', 'model', 'dataset'])
+    return df
 
 
-PREFIX = Path('/home/cgsdfc/Metrics/Eval/data/v2/plot/distplot')
+def make_figures(df: DataFrame, prefix: Path):
+    for metric, df2 in df.groupby('metric'):
+        output = get_output(prefix, metric)
+        metric_norm = normalize_name('metric', metric)
+        caption = '{} 在所有模型和数据集上的概率分布'.format(metric_norm)
+        label = '{}-dist-all'.format(metric_norm)
+        images = df2.filename.values
+        n = int(math.sqrt(len(images)))
+        if pow(n, 2) != len(images):
+            raise ValueError('{} != {}**2'.format(len(images), pow(n, 2)))
+        figure = make_subfigures(
+            images=images,
+            caption=caption,
+            label=label,
+            n=n,
+        )
+        if not output.parent.exists():
+            output.parent.mkdir(parents=True)
+        output.write_text(figure.dumps())
 
 
-def make_subfigures(image, n=3):
+def make_subfigures(images, caption='', label='', n=3):
     width = 1 / n
     fig = Figure()
+    fig.append(Command('centering'))
+    image_iter = iter(images)
+
     for i in range(n):
         for j in range(n):
-            subfig = SubFigure()
+            subfig = SubFigure(width=NoEscape(f'{width}' + r'\linewidth'))
+            subfig.append(Command('centering'))
+            image = str(next(image_iter))
             subfig.add_image(image)
             if i != 0 and j == 0:
                 fig.append(NewLine())
             fig.append(subfig)
+
+    fig.add_caption(caption)
+    fig.append(Label(Marker(prefix='fig', name=label)))
     return fig
 
 
+PREFIX = Path('/home/cgsdfc/Metrics/Eval/data/v2/')
+
 if __name__ == '__main__':
-    image = '/home/cgsdfc/Metrics/Eval/data/v2/plot/distplot/lsdscc/hred/adem/plot.pdf'
-    doc = Document('subfig')
-    fig = make_subfigures(image, 2)
-    doc.append(fig)
-    print(fig.dumps())
-    doc.generate_pdf()
+    df = load_distplot_data(PREFIX / 'plot' / 'distplot')
+    make_figures(
+        prefix=PREFIX,
+        df=df,
+    )
