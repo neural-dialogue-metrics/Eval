cb6a2d5adbe276e8287a90cfb2ffa3c0bee31cba
cgsdfc <cgsdfc@126.com>
2019-05-20 00:02:27 +0800

    add analysis to BLEU and ADEM.

--- corr/boxplot.py
+++ corr/boxplot.py
@@ -1,12 +1,9 @@
-import seaborn as sns
-import logging
-
-from eval.data import seaborn_setup, get_schema_name, load_system_score
 from pathlib import Path
 
-from eval.normalize import normalize_names_in_df
-from eval.utils import make_parent_dirs
+import seaborn as sns
 from eval.consts import PLOT_FILENAME
+from eval.data import get_schema_name, load_system_score
+from eval.utils import make_parent_dirs
 from pandas import DataFrame
 
 NAME = get_schema_name(__file__)
@@ -18,29 +15,15 @@ def get_output(prefix: Path, metric):
     return make_parent_dirs(prefix / NAME / metric / PLOT_FILENAME)
 
 
-def plot(df: DataFrame, prefix: Path):
-    for metric, df2 in df.groupby('metric'):
-        df2 = normalize_names_in_df(df2)
-        output = get_output(prefix, metric)
-        g = sns.catplot(x='dataset', y='system', hue='model', kind='bar', data=df2)
-        g.set_xlabels('')
-        g.set_ylabels('')
-        g.fig.legends[0].set_title('')
-        logging.info('plotting to {}'.format(output))
-        g.savefig(output)
-
-
 def preprocess() -> DataFrame:
-    df = load_system_score(remove_random_model=True)
-    df = df.sort_values(['model', 'dataset'])
+    df = load_system_score(remove_random_model=True, normalize_name=True)
+    df = df.sort_values(['metric', 'model', 'dataset'])
     return df
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    seaborn_setup()
-    df = preprocess()
-    plot(
-        df=df,
-        prefix=Path('/home/cgsdfc/GraduateDesign/figure')
-    )
+def plot(df: DataFrame):
+    g = sns.catplot(x='dataset', y='system', hue='model', kind='box', data=df)
+    g.set_xlabels('')
+    g.set_ylabels('')
+    g.fig.legends[0].set_title('')
+    return g
