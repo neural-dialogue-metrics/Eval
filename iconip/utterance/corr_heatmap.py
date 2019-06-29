3857f1dabca824195e94f475c427882294122916
cgsdfc <cgsdfc@126.com>
2019-06-29 09:58:19 +0800

    Add titles to the corr heatmap.

--- iconip/utterance/corr_heatmap.py
+++ iconip/utterance/corr_heatmap.py
@@ -12,18 +12,23 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import logging
 import seaborn as sns
+from eval.normalize import normalize_name
 
 
 def plot_heatmap():
     sns.set(font_scale=0.9, color_codes=True, font='Times New Roman')
     feature = load_model_dataset2_feature()
-    plt.gcf().subplots_adjust(bottom=0.18)
     for key, value in feature.items():
         output = SAVE_ROOT / 'plot' / 'heatmap' / key[0] / key[1] / PLOT_FILENAME
         logging.info('plotting to {}'.format(output))
         output = make_parent_dirs(output)
         corr = value.corr()  # use pearson
+        plt.gcf().subplots_adjust(bottom=0.18, right=1.0)
         heatmap(corr)
+
+        model = normalize_name('model', key[0])
+        dataset = normalize_name('dataset', key[1])
+        plt.title('Heatmap of Pearson\'s R for {} on {}'.format(model, dataset))
         plt.savefig(output)
         plt.close('all')
 
