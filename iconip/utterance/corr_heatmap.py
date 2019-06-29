f9d59f8b94b873d172392221d51fbdf3df3b03a5
cgsdfc <cgsdfc@126.com>
2019-06-29 09:49:56 +0800

    Perfectly tune the space for corr heatmap

--- iconip/utterance/corr_heatmap.py
+++ iconip/utterance/corr_heatmap.py
@@ -7,13 +7,17 @@ from seaborn import heatmap
 from iconip.utterance import load_model_dataset2_feature, SAVE_ROOT
 from eval.data import seaborn_setup
 from eval.utils import make_parent_dirs
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import logging
+import seaborn as sns
 
 
 def plot_heatmap():
-    seaborn_setup()
+    sns.set(font_scale=0.9, color_codes=True, font='Times New Roman')
     feature = load_model_dataset2_feature()
+    plt.gcf().subplots_adjust(bottom=0.18)
     for key, value in feature.items():
         output = SAVE_ROOT / 'plot' / 'heatmap' / key[0] / key[1] / PLOT_FILENAME
         logging.info('plotting to {}'.format(output))
